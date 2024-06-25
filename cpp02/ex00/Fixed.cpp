55d4ef6faf3213ac12e6fe82945b50065ca8b6ad
asyvash <asyvash@3A1.42angouleme.fr>
2024-06-25 17:45:29 +0200

    filled Makefiles, mains etc

---
+++ cpp02/ex00/Fixed.cpp
@@ -0,0 +1,12 @@
+#include "Fixed.hpp"
+#include <iostream>
+
+Fixed::Fixed() : value(0)
+{
+    std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::~Fixed()
+{
+    std::cout << "Destructor called" << std::endl;
+}
---
+++ cpp02/ex02/Fixed.hpp
@@ -0,0 +1,16 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+class Fixed
+{
+    public:
+
+        
+
+    private:
+        int                 value;
+        static const int    bits;
+
+};
+
+#endif
